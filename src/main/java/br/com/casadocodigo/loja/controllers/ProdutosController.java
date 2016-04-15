e3f7d84f3b8af11c5ad4be5fd83e52fe4986bfc1
Fernando <fexx@hotmail.co.uk>
2016-04-15 11:28:28 -0300

    #3 - Criar o cadastro de produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,9 +3,11 @@ package br.com.casadocodigo.loja.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 public class ProdutosController {
@@ -14,8 +16,10 @@ public class ProdutosController {
 	private ProdutoDAO produtoDAO;
 	
 	@RequestMapping("produtos/form")
-	public String form(){
-		return "produtos/form";
+	public ModelAndView form(){
+		ModelAndView modelAndView = new ModelAndView("produtos/form");//informe atraves do contrutor, para qual pagina deve ir 
+		modelAndView.addObject("tipos", TipoPreco.values()); // para os valores para a view form
+		return modelAndView;
 	}
 	
 	@RequestMapping("/produtos")
