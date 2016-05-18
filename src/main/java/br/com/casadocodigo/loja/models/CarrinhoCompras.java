5fa29973cd601533d244cacab3724b99462d68f0
Fernando <fexx@hotmail.co.uk>
2016-05-18 15:31:07 -0300

    #11 - Criar escopo de sessão

--- src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -3,18 +3,19 @@ package br.com.casadocodigo.loja.models;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component //deixamos ele disponivel para ser injeto pelo spring em outras classes, usando o @Autowired
+@Scope(value=WebApplicationContext.SCOPE_SESSION) //todo bean do spring é um singleton, estamos mudando o scopo default do spring que é o scope_application para scope_session ou seja, cada navegar vai ter uma sessão
 public class CarrinhoCompras {
 
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<>();
 	
-	
 	public void add(CarrinhoItem item){
 		itens.put(item, getQuantidade(item) + 1);
 	}
-
 
 	private int getQuantidade(CarrinhoItem item) {
 		if(!itens.containsKey(item)){
